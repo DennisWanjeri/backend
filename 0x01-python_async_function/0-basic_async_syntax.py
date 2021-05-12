495d5029240ecce3881b19a9da89f9784fcd0e13
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-12 13:45:46 +0300

    asyncio

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random that waits
+for a random delay between 0 and max_delay (included and float value)
+seconds and eventually returns it.
+Use the random module.
+"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for random seconds then return number of seconds awaited
+"""
+    seconds = random.uniform(0, max_delay)
+    await asyncio.sleep(seconds)
+    return seconds
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,29 @@
+#!/data/data/com.termux/files/usr/bin/env python3
+"""Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
+
+import asyncio
+from typing import List
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Make a list of time in second(s)
+    that each task took to complete
+    Parameters
+    ----------
+    n : int
+        number of task(s) to execute concurrently
+    max_delay : int
+        max delaying(sleep) time in second(s)
+    Returns
+    -------
+    list(float)
+        a sorted list of seconds each task took to complete
+    """
+    task_wait_random = __import__('3-tasks').task_wait_random
+    delay_time = [task_wait_random(max_delay) for i in range(n)]
+    complete = []
+
+    for task in asyncio.as_completed(delay_time):
+        complete.append(await task)
+    return complete
