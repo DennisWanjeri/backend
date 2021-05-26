53b5bc08b2c8106c5c249bf1b6a7dcddb531a876
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-26 09:15:42 +0300

    user authentication service

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -70,12 +70,23 @@ class Auth:
         except NoResultFound:
             pass
 
-    def get_reset_password_token(email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """resetting password"""
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
+        except NoResultFound:
+            raise ValueError
+        
+    def update_password(self, reset_token: str, password: str):
+        """
+        update password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            password = _hash_password(password)
+            self._db.update_user(user.id, reset_token=None, password=password)
         except NoResultFound:
             raise ValueError
